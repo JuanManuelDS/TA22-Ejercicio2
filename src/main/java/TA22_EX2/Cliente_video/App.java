6ea31d0b2536876c32694b991d1570ecd3d7c528
JuanManuelDS <juanmadesenzi@gmail.com>
2022-11-07 09:19:43 +0100

    primer commit

---
+++ src/main/java/TA22_EX2/Cliente_video/App.java
@@ -0,0 +1,13 @@
+package TA22_EX2.Cliente_video;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TA22_EX2/Cliente_video/AppTest.java
@@ -0,0 +1,20 @@
+package TA22_EX2.Cliente_video;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
